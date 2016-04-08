8b3e65d45c8c9d995ab89d4e3c6936451d454e1e
jcut <ramtinova@gmail.com>
2016-04-08 20:42:17 +0430

    go for

---
+++ journey1/for.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+
+	}
+
+	for j:=3 ; j<=9 ; j++ {
+		fmt.Println(j)
+
+	}
+
+	for {
+		fmt.Println("Looping In Tha Hood")
+		break
+	}
+
+
+
+}
